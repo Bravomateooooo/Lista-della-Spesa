201cf601e2845f1e3c4f23237423e0593d3fac9f
Facciorusso Matteo <matteofac07@gmail.com>
2025-04-24 20:28:43 +0200

    Aggiunta gestione del menu con switch e opzioni per aggiungere, rimuovere, mostrare, ordinare e cercare elementi

--- Index.js
+++ Index.js
@@ -16,5 +16,40 @@ while(continua)
     console.log("5. Cerca elemento");
     console.log("6. Esci"); 
 
-    
+    const scelta=prompt("Scegli un'opzione: ");
+
+    switch(scelta)
+    {
+        case "1":
+            const aggiungi=prompt("Inserisci l'elemento da aggiungere: ");
+            aggiungiElemento(aggiungi);
+            break;
+
+        case "2":
+            const rimuovi=prompt("Inserisci l'elemento da rimuovere: ");
+            rimuoviElemento(rimuovi);
+            break;
+            
+        case "3":
+            mostraLista();
+            break;
+           
+        case "4":
+            ordinaLista();
+            break;
+           
+        case "5":
+            const cerca=prompt("Inserisci il testo da cercare: ");
+            cercaElemento(cerca);
+            break;
+            
+        case "6":
+            continua=false;
+            console.log("Uscita...");
+            break;
+            
+        default:
+            console.log("Opzione non valida, riprova.");
+
+    }
 }
