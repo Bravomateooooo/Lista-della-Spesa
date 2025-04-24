3aba695bb054e5f86c7069767615972ca0233226
Facciorusso Matteo <matteofac07@gmail.com>
2025-04-24 20:23:18 +0200

    Implementato menu interattivo per la gestione della lista della spesa

--- Index.js
+++ Index.js
@@ -5,3 +5,16 @@ const prompt=require("prompt-sync")();
 const {aggiungiElemento, rimuoviElemento, mostraLista, ordinaLista, cercaElemento}=require("./Utils.js");
 
 let continua=true;
+
+while(continua)
+{
+    console.log("\n--- Gestione Lista della Spesa ---");
+    console.log("1. Aggiungi elemento");
+    console.log("2. Rimuovi elemento");
+    console.log("3. Mostra lista");
+    console.log("4. Ordina lista");
+    console.log("5. Cerca elemento");
+    console.log("6. Esci"); 
+
+    
+}
